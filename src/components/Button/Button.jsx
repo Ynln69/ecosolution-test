44e79a8912928380d0f8df1f03d79ef790d89893
Yana <yanalunina96@gmail.com>
2023-12-20 12:18:53 +0200

    Update Button.jsx
    add type to button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import { Btn } from "./Button.styled";
 
 const Button = ({ title, onClick }) => {
   return (
-    <Btn onClick={onClick}>
+    <Btn onClick={onClick} type="button">
       {title}
       <svg width={16} height={16}>
         <use xlinkHref={`${sprite}#icon-circleArrow`}></use>
